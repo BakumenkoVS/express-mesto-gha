2ad13c669c4e16dd0a5f3764854e5daec620a6fb
BakumenkoVS <tinyyy11@gmail.com>
2022-04-28 11:00:19 +0300

    add updateProfile, updateAvatar

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const { getUsers, getUser, createUser } = require('../controllers/user');
+const { getUsers, getUser, createUser, updateProfile, updateAvatar } = require('../controllers/user');
 
 router.get('/', getUsers);
 router.get('/:userId', getUser);
 router.post('/', createUser);
+router.post('/me', updateProfile);
+router.post('/me/avatar', updateAvatar);
 
 module.exports = router;
