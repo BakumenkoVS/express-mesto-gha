07c2ec6e7aa87a965f88bf0d28f1a8cbf3c4901f
BakumenkoVS <tinyyy11@gmail.com>
2022-05-19 16:53:30 +0300

    123

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-
+require('dotenv').config();
 const UnauthorizedError = require('../errors/unauthorizedError');
 const ValidationError = require('../errors/validationError');
 const NotFoundError = require('../errors/notFoundError');
